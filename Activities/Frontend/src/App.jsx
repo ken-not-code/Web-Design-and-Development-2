a4cd512b2da3348c4573adfb7380eee3721b45dd
Ken Saldua <ken.saldua@ciit.edu.ph>
2026-02-04 13:23:02 +0800

    working with login page

--- Activities/Frontend/src/App.jsx
+++ Activities/Frontend/src/App.jsx
@@ -1,9 +1,48 @@
-import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
 
-  return <div>YHO</div>;
+  useEffect(() => {
+    const fecthUser = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const res = await axios.get("/api/auth/me", {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          setUser(res.data);
+        } catch (err) {
+          setError("failed to fetch user data");
+          localStorage.removeItem("token");
+        }
+      }
+    };
+    fecthUser();
+  }, []);
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ Activities/Frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md border border-gray-300">
+        <h2 className="test-2xl font-bold mb-6 text-center text-gray-800">
+          Login
+        </h2>
+      </div>
+    </div>
+  );
+};
+export default Login;
